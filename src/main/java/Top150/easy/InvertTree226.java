4866ab9b1244b210d78c1ad1e74954a8dc4c89ae
rkm09 <raksha.mishra09@gmail.com>
2023-12-24 21:10:41 +0530

    invert tree

---
+++ src/main/java/Top150/easy/InvertTree226.java
@@ -0,0 +1,57 @@
+package Top150.easy;
+
+import GenDS.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class InvertTree226 {
+    public static void main(String[] args) {
+        TreeNode left = new TreeNode(1);
+        TreeNode right = new TreeNode(3);
+        TreeNode root = new TreeNode(2, left, right);
+        System.out.println(invertTree(root).left.val);
+    }
+
+//    [def]; recursive; time: O(n), space: O(n)
+    public static TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode node = new TreeNode(root.val);
+        node.left = invertTree(root.right);
+        node.right = invertTree(root.left);
+        return node;
+    }
+
+//    iterative; time: O(n), space: O(n)
+    public static TreeNode invertTree1(TreeNode root) {
+        if(root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if(node.left != null) {
+                queue.offer(node.left);
+            }
+            if(node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+}
+/*
+Given the root of a binary tree, invert the tree, and return its root.
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+Input: root = []
+Output: []
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ */
