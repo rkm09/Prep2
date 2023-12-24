36e1693c20600b5717887a472bf998c134bcce66
rkm09 <raksha.mishra09@gmail.com>
2023-12-24 18:42:20 +0530

    same tree

--- src/main/java/Top150/easy/SameTree100.java
+++ src/main/java/Top150/easy/SameTree100.java
@@ -1,10 +1,17 @@
 package Top150.easy;
 
-import javax.swing.tree.TreeNode;
+
+import GenDS.TreeNode;
 
 public class SameTree100 {
     public static void main(String[] args) {
-
+        TreeNode left = new TreeNode(2);
+        TreeNode right = new TreeNode(3);
+        TreeNode p = new TreeNode(1, left, right);
+        TreeNode left1 = new TreeNode(2);
+        TreeNode right1 = new TreeNode(3);
+        TreeNode q = new TreeNode(1, left1, right1);
+        System.out.println(isSameTree(p, q));
     }
     public static boolean isSameTree(TreeNode p, TreeNode q) {
         return false;
