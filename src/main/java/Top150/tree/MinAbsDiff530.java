6d634025e839c51d6010d9a7c8f74d9575f21e6a
rkm09 <raksha.mishra09@gmail.com>
2023-12-30 12:41:57 +0530

    min diff bst

--- src/main/java/Top150/tree/MinAbsDiff530.java
+++ src/main/java/Top150/tree/MinAbsDiff530.java
@@ -8,14 +8,71 @@ import java.util.List;
 import java.util.Stack;
 
 public class MinAbsDiff530 {
+    static List<Integer> countNodes = new ArrayList<>();
+    static TreeNode prevNode;
+    static int minDifference = Integer.MAX_VALUE;
     public static void main(String[] args) {
         TreeNode left1 = new TreeNode(1);
         TreeNode left2 = new TreeNode(3);
         TreeNode left = new TreeNode(2, left1, left2);
         TreeNode right = new TreeNode(6);
         TreeNode root = new TreeNode(4, left, right);
-        System.out.println(getMinimumDifference(root));
+        System.out.println(getMinimumDifference3(root));
     }
+
+//    inorder traversal without list; time: O(n), space: O(n)
+    public static int getMinimumDifference3(TreeNode root) {
+        inorderTraversal(root);
+        return minDifference;
+    }
+    private static void inorderTraversal(TreeNode node) {
+        if(node == null) return;
+        inorderTraversal(node.left);
+        if(prevNode != null) {
+            minDifference = Math.min(node.val - prevNode.val, minDifference);
+        }
+        prevNode = node;
+        inorderTraversal(node.right);
+    }
+
+//    inorder traversal using list; time: O(n), space: O(n)
+//    BST inorder is already sorted
+    public static int getMinimumDifference2(TreeNode root) {
+        inorder(root);
+        int n = countNodes.size();
+        int minDiff = Integer.MAX_VALUE;
+        for(int i = 1; i < n ; i++) {
+            minDiff = Math.min(minDiff, countNodes.get(i) - countNodes.get(i-1));
+        }
+        return minDiff;
+    }
+    private static void inorder(TreeNode node) {
+        if(node == null) return;
+        inorder(node.left);
+        countNodes.add(node.val);
+        inorder(node.right);
+    }
+
+//    recursive dfs; time: O(nlogn), space: O(n)
+    public static int getMinimumDifference1(TreeNode root) {
+        dfs(root);
+        System.out.println(countNodes);
+        Collections.sort(countNodes);
+        int n = countNodes.size();
+        int minDiff = Integer.MAX_VALUE;
+        for(int i = 1; i < n ; i++) {
+            minDiff = Math.min(minDiff, countNodes.get(i) - countNodes.get(i-1));
+        }
+        return minDiff;
+    }
+    private static void dfs(TreeNode node) {
+        if(node == null) return;
+        countNodes.add(node.val);
+        dfs(node.left);
+        dfs(node.right);
+    }
+
+//    [def]; iterative with stack; time: O(nlogn)
     public static int getMinimumDifference(TreeNode root) {
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
@@ -33,7 +90,7 @@ public class MinAbsDiff530 {
         Collections.sort(li);
         int res = Integer.MAX_VALUE;
         for(int i = 1 ; i < li.size() ; i++) {
-            res = Math.min(res, Math.abs(li.get(i) - li.get(i-1)));
+            res = Math.min(res, li.get(i) - li.get(i-1));
         }
         return res;
     }
@@ -48,4 +105,6 @@ Output: 1
 Constraints:
 The number of nodes in the tree is in the range [2, 104].
 0 <= Node.val <= 105
+
+same as leet 783.
  */
