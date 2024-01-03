f0bff89a3cfcb134a81430364114957c02bb4e18
rkm09 <raksha.mishra09@gmail.com>
2024-01-03 09:11:11 +0530

    kth largest

--- src/main/java/Top150/heap/kthLargest215.java
+++ src/main/java/Top150/heap/kthLargest215.java
@@ -1,18 +1,15 @@
 package Top150.heap;
 
-import GenDS.TreeNode;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
+import java.util.*;
 
 public class kthLargest215 {
     public static void main(String[] args) {
         int[] nums = {3,2,1,5,6,4}; int k = 2;
-        System.out.println(findKthLargest(nums, k));
+        System.out.println(findKthLargest1(nums, k));
     }
 
-//    frequency counter; time: O(n), space: O(n)
+//  Counting sort [frequency counter]; time: O(m+n), space: O(m) [fast]
     public static int findKthLargest(int[] nums, int k) {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
@@ -33,8 +30,54 @@ public class kthLargest215 {
         return -1;
     }
 
+//    quick select; time: O(n) [worst case O(n^2) but highly unlikely], space: O(n)
+//    ps: in general quick select algo is used for kth smallest, to reverse this...we fill "Greater than" into "left"..
+    public static int findKthLargest1(int[] nums, int k) {
+        List<Integer> list = new ArrayList<>();
+        for(int num : nums) {
+            list.add(num);
+        }
+        return quickSelect(list, k);
+    }
+    private static int quickSelect(List<Integer> nums, int k) {
+        int pivotIndex = new Random().nextInt(nums.size());
+        int pivot = nums.get(pivotIndex);
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
+        for(int num : nums) {
+            if(num > pivot) {
+                left.add(num);
+            } else if(num < pivot) {
+                right.add(num);
+            } else {
+                mid.add(num);
+            }
+        }
+        if(k <= left.size()) {
+            return quickSelect(left, k);
+        }
+        if(left.size() + mid.size() < k) {
+            return quickSelect(right, k - left.size() - mid.size());
+        }
+        return pivot;
+    }
+
+//    min heap using priority queue; time: O(nlogk), space: O(k) where k is the heap size [slow]
+    public static int findKthLargest2(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for(int num : nums) {
+            heap.add(num);
+            if(heap.size() > k) {
+                heap.remove();
+            }
+        }
+        return heap.peek();
+    }
+
+
 //    time: O(nlogn), space: O(logn)
-    public static int findKthLargestX(int[] nums, int k) {
+    public static int findKthLargest3(int[] nums, int k) {
         Arrays.sort(nums);
         return nums[nums.length - k];
     }
