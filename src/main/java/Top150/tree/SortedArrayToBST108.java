93ef09ee3f3e779ae8f9ad866a2af2e11554e116
rkm09 <raksha.mishra09@gmail.com>
2024-01-09 12:22:13 +0530

    sorted array to bst

--- src/main/java/Top150/tree/SortedArrayToBST108.java
+++ src/main/java/Top150/tree/SortedArrayToBST108.java
@@ -2,23 +2,49 @@ package Top150.tree;
 
 import GenDS.TreeNode;
 
+import java.util.Random;
+
 public class SortedArrayToBST108 {
+    static Random rand = new Random();
     public static void main(String[] args) {
         int[] nums = {-10,-3,0,5,9};
         System.out.println(sortedArrayToBST(nums));
     }
+
+//    time: O(n), space: O(logn)
     public static TreeNode sortedArrayToBST(int[] nums) {
         final int n = nums.length;
-        int mid = nums[n/2];
-        TreeNode root = new TreeNode(mid);
-        if(n == 1) return root;
-        int i = mid - 1, j = mid + 1;
-        while(i >= 0 && j < n) {
-            TreeNode left = new TreeNode(nums[i]);
-            TreeNode right = new TreeNode(nums[j]);
-            root.left = left;
-            root.right = right;
-        }
+        return helper(nums, 0, n-1);
+    }
+    //    Preorder traversal (always choose left elem as mid);
+    private static TreeNode helper(int[] nums, int left, int right) {
+        if(left > right) return null;
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+
+    //    Preorder traversal (always choose right elem as mid);
+    private static TreeNode helper1(int[] nums, int left, int right) {
+        if(left > right) return null;
+        int mid = left + (right - left) / 2;
+        if((left + right) % 2 == 1) mid++;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+
+    //    Preorder traversal (choose random elem as mid);
+    private static TreeNode helper2(int[] nums, int left, int right) {
+        if(left > right) return null;
+        int mid = left + (right - left) / 2;
+        if((left + right) % 2 == 1) mid += rand.nextInt(2);
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
         return root;
     }
 }
@@ -35,4 +61,10 @@ Constraints:
 1 <= nums.length <= 104
 -104 <= nums[i] <= 104
 nums is sorted in a strictly increasing order.
+--------------------------------------------
+Tree Traversals: DFS(preorder, inorder, postorder), BFS
+Inorder traversal of BST is an array sorted in ascending order. However, inorder traversal does not uniquely identify a BST.
+But postorder & preorder do.
+Height Balanced => the depths of the two sub trees of every node never differ by more than one.
+=> take the mid element as root each time.
  */
