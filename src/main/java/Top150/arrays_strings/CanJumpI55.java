fedd75cef547cfb473110f331def930216c949b4
rkm09 <raksha.mishra09@gmail.com>
2024-01-12 19:33:21 +0530

    can jump I

--- src/main/java/Top150/arrays_strings/CanJumpI55.java
+++ src/main/java/Top150/arrays_strings/CanJumpI55.java
@@ -6,11 +6,65 @@ public class CanJumpI55 {
     public static void main(String[] args) {
 //        int[] nums = {2,3,1,1,4};
         int[] nums = {2,5,0,0};
-        System.out.println(canJump(nums));
+        System.out.println(canJump2(nums));
     }
 
-//    [def]; dp
+//    greedy(like a constant space bottom up dp); time: O(n), space: O(1); fastest;
     public static boolean canJump(int[] nums) {
+        int n = nums.length;
+        int lastPos = n - 1;
+        for(int i = lastPos - 1; i >= 0 ; i--) {
+            if(i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+
+//    refined bottom up dp; time: O(n^2), space: O(n)
+    public static boolean canJump2(int[] nums) {
+        int n = nums.length;
+        boolean[] dp = new boolean[n];
+        dp[n-1] = true;
+        for(int i = n - 2 ; i >= 0 ; i--) {
+            int furthest = Math.min(i + nums[i], n - 1);
+            for(int j = i + 1 ; j <= furthest ; j++) {
+                if(dp[j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+
+    enum Index {
+        GOOD, BAD, UNKNOWN
+    }
+
+    //        another using enum; bottom up dp; time: O(n^2), space: O(n)
+    public static boolean canJump3(int[] nums) {
+        int n = nums.length;
+        Index[] dp = new Index[n];
+        for(int i = 0 ; i < n ; i++) {
+            dp[i] = Index.UNKNOWN;
+        }
+        dp[n-1] = Index.GOOD;
+        for(int i = n - 2; i >= 0 ; i--) {
+            int furthest = Math.min(i + nums[i], n-1);
+            for(int j = i + 1 ; j <= furthest ; j++) {
+                if(dp[j] == Index.GOOD) {
+                    dp[i] = Index.GOOD;
+                    break;
+                }
+            }
+        }
+        return dp[0] == Index.GOOD;
+    }
+
+
+    //    [def]; bottom up dp; time: O(n^2), space: O(n)
+    public static boolean canJump1(int[] nums) {
         int n = nums.length;
         boolean[] dp = new boolean[n];
         dp[n-1] = true;
