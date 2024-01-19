8ca8ad64cc3b91d9a2bbf79f2988b295fb43f53f
rkm09 <raksha.mishra09@gmail.com>
2024-01-19 09:32:33 +0530

    min path sum

--- src/main/java/LeetDaily/medium/MinFallingPathSum931.java
+++ src/main/java/LeetDaily/medium/MinFallingPathSum931.java
@@ -3,27 +3,58 @@ package LeetDaily.medium;
 public class MinFallingPathSum931 {
     public static void main(String[] args) {
         int[][] matrix = {{2,1,3},{6,5,4},{7,8,9}};
-        System.out.println(minFallingPathSumX(matrix));
+        System.out.println(minFallingPathSum2(matrix));
+    }
+
+//    top down dp with memoization (dfs approach); time: O(n^2), space: O(n)
+    public static int minFallingPathSum2(int[][] matrix) {
+        int minPathSum = Integer.MAX_VALUE;
+        Integer[][] memo = new Integer[matrix.length][matrix[0].length];
+        for(int col = 0 ; col < matrix.length ; col++) {
+            minPathSum = Math.min(findMinPathSum(matrix, 0, col, memo), minPathSum);
+        }
+        return minPathSum;
+    }
+    private static int findMinPathSum(int[][] matrix, int row, int col, Integer[][] memo) {
+//        base case
+        if(col < 0 || col == matrix[0].length) {
+            return Integer.MAX_VALUE;
+        }
+//        reached the end; stop and return;
+        if(row == matrix.length - 1) {
+            return matrix[row][col];
+        }
+//        return from memo
+        if(memo[row][col] != null) {
+            return memo[row][col];
+        }
+
+        int left = findMinPathSum(matrix, row + 1, col - 1, memo);
+        int middle = findMinPathSum(matrix, row + 1, col, memo);
+        int right = findMinPathSum(matrix, row + 1, col + 1, memo);
+
+        memo[row][col] = Math.min(left, Math.min(middle, right)) + matrix[row][col];
+        return memo[row][col];
     }
 
 //    brute force dfs; TLE; time: O(N.3^N), space: O(N)
     public static int minFallingPathSumX(int[][] matrix) {
         int minPathSum = Integer.MAX_VALUE;
         for(int col = 0 ; col < matrix.length ; col++) {
-            minPathSum = Math.min(minPathSum, findMinFallingPath(matrix, 0, col));
+            minPathSum = Math.min(minPathSum, findMinFallingPathX(matrix, 0, col));
         }
         return minPathSum;
     }
-    private static int findMinFallingPath(int[][] matrix, int row, int col) {
-        if(col < 0 || col >= matrix[0].length) {
+    private static int findMinFallingPathX(int[][] matrix, int row, int col) {
+        if(col < 0 || col == matrix[0].length) {
             return Integer.MAX_VALUE;
         }
         if(row == matrix.length - 1) {
             return matrix[row][col];
         }
-        int left = findMinFallingPath(matrix, row + 1, col - 1);
-        int middle = findMinFallingPath(matrix, row + 1, col);
-        int right = findMinFallingPath(matrix, row + 1, col + 1);
+        int left = findMinFallingPathX(matrix, row + 1, col - 1);
+        int middle = findMinFallingPathX(matrix, row + 1, col);
+        int right = findMinFallingPathX(matrix, row + 1, col + 1);
 
         return Math.min(left, Math.min(middle, right)) + matrix[row][col];
     }
