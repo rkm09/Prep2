c92a61b1641a32bb328daaac5c947637aa9bf075
rkm09 <raksha.mishra09@gmail.com>
2024-01-30 20:29:58 +0530

    validate bst

--- src/main/java/Top150/tree/ValidateBST98.java
+++ src/main/java/Top150/tree/ValidateBST98.java
@@ -8,16 +8,73 @@ import java.util.Deque;
 import java.util.LinkedList;
 
 public class ValidateBST98 {
+    private static Integer prev;
     public static void main(String[] args) {
         TreeNode left = new TreeNode(4);
         TreeNode right1 = new TreeNode(3);
         TreeNode right2 = new TreeNode(7);
         TreeNode right = new TreeNode(6, right1, right2);
         TreeNode root = new TreeNode(5, left, right);
-        System.out.println(isValidBST1(root));
+        System.out.println(isValidBST(root));
     }
 
+//    inorder recursion; time: O(n), space: O(n); fast
+//    postOrder dfs: Left-Right-Root [bottom to top, left to right], preorder dfs: Root-Left-Right [top to bottom, left to right]
+//    inorder dfs: Left-Root-Right, bfs : [top to bottom, left to right]
+//    inorder bst traversal naturally ensures ascending order
+    public static boolean isValidBST(TreeNode root) {
+        prev = null;
+        return inorder(root);
+    }
+    private static boolean inorder(TreeNode node) {
+        if(node == null) {
+            return true;
+        }
+        if(!inorder(node.left)) {
+            return false;
+        }
+        if(prev != null && node.val <= prev) {
+            return false;
+        }
+        prev = node.val;
+        return inorder(node.right);
+    }
+
+//    inorder iterative; time: O(n), space: O(n)
     public static boolean isValidBST1(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        Integer prev = null;
+        while(!stack.isEmpty() || root != null) {
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if(prev != null && root.val <= prev) {
+                return false;
+            }
+            prev = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+
+
+    //    recursion; time: O(n), space: O(n); fast
+    public static boolean isValidBST2(TreeNode root) {
+        return validate(root, null, null);
+    }
+    private static boolean validate(TreeNode node, Integer low, Integer high) {
+        if(node == null)
+            return true;
+        if((low != null && node.val <= low) || (high != null && node.val >= high)) {
+            return false;
+        }
+        return validate(node.right, node.val, high) && validate(node.left, low, node.val);
+    }
+
+//    iterative with stack; time: O(n), space: O(n)
+    public static boolean isValidBST3(TreeNode root) {
         boolean isValid = true;
         Deque<TreeNode> stack = new LinkedList<>() {{ push(root); }};
         Deque<Pair<Integer, Integer>> stackLimits = new LinkedList<>() {{ push(null); push(null); }};
@@ -39,18 +96,7 @@ public class ValidateBST98 {
         return isValid;
     }
 
-//    recursion; time: O(n), space: O(n)
-    public static boolean isValidBST(TreeNode root) {
-         return validate(root, null, null);
-    }
-    private static boolean validate(TreeNode node, Integer low, Integer high) {
-        if(node == null)
-            return true;
-        if((low != null && node.val <= low) || (high != null && node.val >= high)) {
-            return false;
-        }
-        return validate(node.right, node.val, high) && validate(node.left, low, node.val);
-    }
+
 }
 
 /*
