406a1104279863655324ba0ad00343984e7dcb6c
rkm09 <raksha.mishra09@gmail.com>
2024-02-01 14:35:32 +0530

    partition

---
+++ src/main/java/LeetDaily/medium/DivideArray2966.java
@@ -0,0 +1,53 @@
+package LeetDaily.medium;
+
+import java.util.Arrays;
+
+public class DivideArray2966 {
+    public static void main(String[] args) {
+        int[] nums = {1,3,4,8,7,9,3,5,1};
+        int[][] res = divideArray(nums, 2);
+        for(int[] ar : res) {
+            System.out.println(Arrays.toString(ar));
+        }
+    }
+
+//    sorting; time: O(nlogn), space: O(n)
+    public static int[][] divideArray(int[] nums, int k) {
+        int n = nums.length;
+        int[][] ans = new int[n/3][3];
+        Arrays.sort(nums);
+        for(int i = 0 ; i < n ; i += 3)
+        {
+            if(nums[i + 2] - nums[i] > k) {
+                return new int[0][0];
+            }
+            ans[i / 3] = new int[] {nums[i], nums[i + 1], nums[i + 2]};
+            System.out.println(i);
+        }
+        return ans;
+    }
+}
+
+/*
+You are given an integer array nums of size n and a positive integer k.
+Divide the array into one or more arrays of size 3 satisfying the following conditions:
+Each element of nums should be in exactly one array.
+The difference between any two elements in one array is less than or equal to k.
+Return a 2D array containing all the arrays. If it is impossible to satisfy the conditions, return an empty array. And if there are multiple answers, return any of them.
+Example 1:
+Input: nums = [1,3,4,8,7,9,3,5,1], k = 2
+Output: [[1,1,3],[3,4,5],[7,8,9]]
+Explanation: We can divide the array into the following arrays: [1,1,3], [3,4,5] and [7,8,9].
+The difference between any two elements in each array is less than or equal to 2.
+Note that the order of elements is not important.
+Example 2:
+Input: nums = [1,3,3,2,7,3], k = 3
+Output: []
+Explanation: It is not possible to divide the array satisfying all the conditions.
+Constraints:
+n == nums.length
+1 <= n <= 105
+n is a multiple of 3.
+1 <= nums[i] <= 105
+1 <= k <= 105
+ */
